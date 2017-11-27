57800a6d4477f996d2f4d7fe6eb93e9daac8759a
StoneArk <stoneark86@gmail.com>
2017-11-27 13:57:30 +0800

    Tricks to avoid runtime error due to testcase input illegal. Code refine.

--- spoj16892.swift
+++ spoj16892.swift
@@ -1,70 +1,74 @@
 // http://www.spoj.com/problems/SPTTRN1/
 // SPTTRN1 - Straight Line Spiral Pattern (Act 1)
 
+enum Direction: Int {
+    case right, down, left, top
+    static func count() -> Int{
+        return Direction.top.rawValue + 1
+    }
+    func next() -> Direction {
+        return Direction.init(rawValue: (self.rawValue + 1) % Direction.count())!
+    }
+}
+
+struct Pointer {
+    var x = 0
+    var y = 0
+    var boundaryRight = 0
+    var boundaryBottom = 0
+    var boundaryLeft = 0
+    var boundaryTop = 0
+    
+    func offset(by direction: Direction) -> Pointer {
+        var pointer = self
+        switch direction {
+        case .right:
+            pointer.y = pointer.y + 1
+        case .down:
+            pointer.x = pointer.x + 1
+        case .left:
+            pointer.y = pointer.y - 1
+        case .top:
+            pointer.x = pointer.x - 1
+        }
+        return pointer
+    }
+    
+    mutating func checkDirection(direction: inout Direction) {
+        switch direction {
+        case .right:
+            if y >= boundaryRight {
+                direction = direction.next()
+                boundaryRight = boundaryRight - 2
+            }
+        case .down:
+            if x >= boundaryBottom {
+                direction = direction.next()
+                boundaryBottom = boundaryBottom - 2
+            }
+        case .left:
+            if y <= boundaryLeft {
+                direction = direction.next()
+                boundaryLeft = boundaryLeft + 2
+            }
+        case .top:
+            if x <= boundaryTop {
+                direction = direction.next()
+                boundaryTop = boundaryTop + 2
+            }
+        }
+    }
+}
+
 let t = Int(readLine()!)!
 for index in 0..<t {
-    let s = Int(readLine()!)!
+    // Inputs of some testcases include illegals. Some line may have extra spaces, like "11 ", leading runtime error when transforming from String to Int.
+    // So here do some tricks to avoid.
+    let testcase = readLine()!
+    let parameters = testcase.characters.split(separator: " ").map(String.init)
+    let s = Int(parameters[0])!
     var matrix: [[Character]] = Array.init(repeating: Array.init(repeating: ".", count: s), count: s)
-
-    enum Direction: Int {
-        case right, down, left, top
-        static func count() -> Int{
-            return Direction.top.rawValue + 1
-        }
-        func next() -> Direction {
-            return Direction.init(rawValue: (self.rawValue + 1) % Direction.count())!
-        }
-    }
-
     var dir: Direction = .right
-    struct Pointer {
-        var x = 0
-        var y = 0
-        var boundaryRight = 0
-        var boundaryBottom = 0
-        var boundaryLeft = 0
-        var boundaryTop = 0
-        
-        func offset(by direction: Direction) -> Pointer {
-            var pointer = self
-            switch direction {
-            case .right:
-                pointer.y = pointer.y + 1
-            case .down:
-                pointer.x = pointer.x + 1
-            case .left:
-                pointer.y = pointer.y - 1
-            case .top:
-                pointer.x = pointer.x - 1
-            }
-            return pointer
-        }
-        
-        mutating func checkDirection(direction: inout Direction) {
-            switch direction {
-            case .right:
-                if y >= boundaryRight {
-                    direction = direction.next()
-                    boundaryRight = boundaryRight - 2
-                }
-            case .down:
-                if x >= boundaryBottom {
-                    direction = direction.next()
-                    boundaryBottom = boundaryBottom - 2
-                }
-            case .left:
-                if y <= boundaryLeft {
-                    direction = direction.next()
-                    boundaryLeft = boundaryLeft + 2
-                }
-            case .top:
-                if x <= boundaryTop {
-                    direction = direction.next()
-                    boundaryTop = boundaryTop + 2
-                }
-            }
-        }
-    }
 
     func checkPointerLegal(pointer: Pointer) -> Bool {
         return pointer.x >= 0 && pointer.x < s && pointer.y >= 0 && pointer.y < s
